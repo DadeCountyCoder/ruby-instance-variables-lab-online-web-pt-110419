b778770bf6fa44b3241c6aab4ffa5f993d7a59dd
Alfredo Diaz <mail4alfredo@gmail.com>
2019-12-04 21:27:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,19 @@
 class Dog
-  def initialize(name)
-    @name = name
   
+  def name=(dogs_name)  
+  @this_dogs_name =
+  dogs_name
+  end
+
+  def name
+    @this_dogs_name
   end
 end
-  lassie = Dog.new
+
+
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+
+
+puts lassie.name
