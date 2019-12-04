588d9d6a6562c6161482acaef99a3e7745a4c564
Alfredo Diaz <mail4alfredo@gmail.com>
2019-12-04 21:30:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,14 +5,14 @@ class Dog
   dogs_name
   end
 
-#   def name
-#     @this_dogs_name
-#   end
-# end
+  def name
+    @this_dogs_name
+  end
+end
 
 
 lassie = Dog.new
-# lassie.name = "Lassie"
+lassie.name = "Lassie"
  
 
 
